7669174588e8f2caa01b34e6539e913bc100c802
Brankica <Brankica@Brankica>
2015-10-09 22:05:03 +0200

    Klasse Spielfeld, Konstruktor, Getter und Setter

--- Dame/src/klassen/Spielfeld.java
+++ Dame/src/klassen/Spielfeld.java
@@ -1,5 +1,40 @@
 package klassen;
-
+/**
+ * Klasse Spielfeld
+ * @author Brankica
+ *
+ */
 public class Spielfeld {
-
+	private int id;
+	private Spielfigur figur;
+	
+	/**
+	 * Konstruktor der Klasse Spielfeld
+	 * @param id ist das Feld auf dem sich die einzelnen Spielfiguren befinden
+	 * @param figur ist die Spielfigur die das Spielfeld besitzt
+	 */
+	public Spielfeld(int id, Spielfigur figur){
+		this.setId(id);
+		this.setFigur(figur);
+	}
+	public int getId(){
+		return id;
+	}
+	public void setId(int id){
+		if(id<0||id>100){
+			throw new RuntimeException("ungültiges Spielfeld");
+		}
+		this.id=id;
+	}
+	public Spielfigur getFigur(){
+		return figur;
+	}
+	public void setFigur(Spielfigur figur){
+		this.figur=figur;
+	}
+	@Override
+	public String toString(){
+		return ("Id: "+getId()+
+				"Figur: "+getFigur());
+	}
 }
