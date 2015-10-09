732b583d7036732e976ea267578781a1077acc6e
MaidSarajlija <Maid.Sarajlija@Student.Reutlingen-University.DE>
2015-10-09 15:00:28 +0200

    Klasse Spieler mit Konstruktor, Getter und Setter

--- Dame/src/klassen/Spieler.java
+++ Dame/src/klassen/Spieler.java
@@ -1,5 +1,57 @@
 package klassen;
-
+/**
+ * Die Klasse Spieler
+ * @author B2
+ *
+ */
 public class Spieler {
+	private String name;
+	private Spielfigur figur;
+	private FarbEnum farbe;
+	
+	/**
+	 * Der Konstruktor der Klasse Spieler
+	 * @param name ist der Name der Spielers
+	 * @param figur steht für die Figuren die der Spieler besitzt
+	 * @param farbe ist die jeweilige Farbe des Spielers
+	 */
+	public Spieler(String name, Spielfigur figur, FarbEnum farbe){
+		this.setFarbe(farbe);
+		this.setFigur(figur);
+		this.setName(name);
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		if((name.length()<2)||(name==null)){
+			throw new RuntimeException("Name ist zu kurz");
+		}
+		this.name = name;
+	}
+	public Spielfigur getFigur() {
+		return figur;
+	}
+	public void setFigur(Spielfigur figur) {
+		this.figur = figur;
+	}
+	public FarbEnum getFarbe() {
+		return farbe;
+	}
+	public void setFarbe(FarbEnum farbe) {
+		this.farbe = farbe;
+	}
+	
+	@Override 
+	public String toString(){
+		return
+				("Name: " + getName() +
+				"Farbe: " + getFarbe() +
+				"Figur: " + getFigur());
+				
+	}
+	
+	
 
 }
