35faf61ae952c49e045ad40bd82b5e1b7431359f
Katherina <Katherina@kathi>
2015-10-13 13:30:15 +0200

    commit

--- Dame/src/TEstMerge.java
+++ Dame/src/TEstMerge.java
@@ -3,7 +3,9 @@ public class TEstMerge {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("dd");
+System.out.println("hallo na was geht "
+		+ "");
+
 	}
 
 }
