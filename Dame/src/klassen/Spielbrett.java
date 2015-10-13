280e1cc1e70e351d3ef0fcf633b87ffa97c04168
Katherina <Katherina@kathi>
2015-10-13 13:15:12 +0200

    spielbrett

--- Dame/src/klassen/Spielbrett.java
+++ Dame/src/klassen/Spielbrett.java
@@ -1,8 +1,47 @@
 package klassen;
 
 public class Spielbrett {
-	
 
-	
+		
+		 private Spielfeld [][] feld;
+		
+
+
+		 public Spielbrett(){
+			 
+			 feld= new Spielfeld[12][12];
+		
+
+			 for (int i = 0; i < feld.length; i++) {
+
+		            for (int  j =0; j <feld[i].length; j++) {
+		            	
+		            	
+		            	
+//		            	feld[i][j]= new Spielfeld();
+
+		    
+		                
+		            System.out.println(feld[i][j]);	
+		            	  
+		            }}
+		 }
+		
+	public static void main(String[] args) {
+		
+		Spielbrett s = new Spielbrett();
+		
+		System.out.println(s);
+	}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
 
 }
