aba96afcf2a43440b1dac8414d920bb2f576fb35
MaidSarajlija <Maid.Sarajlija@Student.Reutlingen-University.DE>
2015-10-13 15:07:16 +0200

    test

--- Dame/src/TEstMerge.java
+++ Dame/src/TEstMerge.java
@@ -2,9 +2,10 @@
 public class TEstMerge {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("dd");
-System.out.println("lol");
+		
+		System.out.println("dd");
+		System.out.println("lol");
+		System.out.println("hallo");
 	}
 
 }
