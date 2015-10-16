193f405a54fc095b07c1f772dace759376f4c51d
Katherina <Katherina@kathi>
2015-10-16 15:34:45 +0200

    spielbrett

--- Dame/src/klassen/Spielbrett.java
+++ Dame/src/klassen/Spielbrett.java
@@ -27,72 +27,15 @@ public class Spielbrett {
 		feld = new Spielfeld[12][12];
 		getSpielbrett(feld);
 	}
-	/**
-	 * Methode zum setzen des Spielbretts und dessen Beschriftung
-	 * 
-	 * @param feld
-	 */
-	public void getSpielbrett(Spielfeld[][] feld) {
-
-		char ch = 'A';
-		char ch1 = 'L';
-		int x = 1;
-		String str=null;
-
-	Spielfeld spielfeld;
-	boolean feldSchwarz;
-
-	/**
-	 * Konstruktor der Klasse Spielbrett
-	 */
-
-	private Spielfeld[][] feld;
-
-	public Spielbrett() {
-
-		feld = new Spielfeld[12][12];
-		getSpielbrett(feld);
-	}
-	/**
-	 * Methode zum setzen des Spielbretts und dessen Beschriftung
-	 * 
-	 * @param feld
-	 */
-	public void getSpielbrett(Spielfeld[][] feld) {
-
-		char ch = 'A';
-		char ch1 = 'L';
-		int x = 1;
-		String str=null;
 	
+	public void getSpielbrett(Spielfeld[][] feld) {
 
-		for (int i = 0; i <feld.length; i++) {
-			for (int j = 0; j <12; j++) {
-				if (j<=12) {
-					str = "" + ch + x;
-//					ch++;
-					x++;
-				}
-				
-//					if (i<feld.length) {
-//
-//						i++;
-//
-//					}
-					Spielfeld k = new Spielfeld(str);
-					System.out.println(k);
-				}	
-			
-		
-			
+		char ch = 'A';
+		char ch1 = 'L';
+		int x = 1;
+		String str=null;
 
-			}	
-		
-
-			}
-
-
-	public Spielbrett() {
+	
 
 			 for (int i = 0; i < feld.length; i++) {
 
@@ -105,64 +48,10 @@ public class Spielbrett {
 		    
 		                
 		            System.out.println(feld[i][j]);	
-		            	  
-		            }}
-	/**
-	 * Methode zum setzen des Spielbretts und dessen Beschriftung
-	 * 
-	 * @param feld
-	 */
-	public void getSpielbrett(Spielfeld[][] feld) {
-
-		char ch = 'A';
-		char ch1 = 'L';
-		int x = 1;
-		String str=null;
+		            }
+			 }
+		            }
 	
-
-		for (int i = 0; i <feld.length; i++) {
-			for (int j = 0; j <12; j++) {
-				if (ch <=ch1) {
-					str = "" + ch + x;
-					ch++;
-					x++;
-				}
-				
-//					if (i<feld.length) {
-//
-//						i++;
-//
-//					}
-					Spielfeld k = new Spielfeld(str);
-					System.out.println(k);
-				}	
-			
-		
-		for (int i = 0; i <feld.length; i++) {
-			for (int j = 0; j <12; j++) {
-				if (ch <=ch1) {
-					str = "" + ch + x;
-					ch++;
-					x++;
-				}
-				
-//					if (i<feld.length) {
-//
-//						i++;
-//
-//					}
-					Spielfeld k = new Spielf
-		feld = new Spielfeld[12][12];
-		getSpielbrett(feld);
-	}
-eld(str);
-					System.out.println(k);
-				}	
-			
- }
-		feld = new Spielfeld[12][12];
-		getSpielbrett(feld);
-	}
 		
 public static void main(String[] args) {
 
