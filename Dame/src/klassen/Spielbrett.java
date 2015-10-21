179e0d2a8a1c5aedff4e5ed913763b0afb84451e
Katherina <Katherina@kathi>
2015-10-21 22:02:55 +0200

    Spielbrett nach Erneuerung wieder die Farben gesetzt und den haschcode enfernt

--- Dame/src/klassen/Spielbrett.java
+++ Dame/src/klassen/Spielbrett.java
@@ -1,6 +1,8 @@
+
+
 package klassen;
 
-
+import java.util.Arrays;
 
 /**
  * Klasse SPielbrett
@@ -10,79 +12,92 @@ package klassen;
  */
 public class Spielbrett {
 
-
-		
-		Spielfeld spielfeld;
-
-
-
-		Spielbrett sb;
-	/**
-	 * Konstruktor der Klasse Spielbrett
-	 */
+	Spielfeld spielfeld;
+	Spielbrett sb;
+	
 
 	private Spielfeld[][] feld;
-	private boolean schwarz=true;
+	private boolean schwarz = false;
+	
+	/**
+	 * Konstruktor der Klasse Spielbrett
+	 * Methode getSpielbrett wird aufgerufe 
+	 * und die 12*12 Spielfelder werden dem Spielbrett uebergeben
+	 * 
+	 */
 
 	public Spielbrett() {
 
 		feld = new Spielfeld[12][12];
 		getSpielbrett(feld);
 	}
-	
-	
-	
+
+	/**
+	 * Methode getSpielbrett 
+	 * Felder werden erstellt mit jeweiliger Beschriftung und 
+	 * deren Farbe
+	 * @param feld
+	 */
 	public void getSpielbrett(Spielfeld[][] feld) {
 
 		char ch = 'L';
 		char ch1 = 'A';
 		int x = 1;
-		String str=null;
-	
+		String str = null;
 
-			 for (int i = 0; i <feld.length; i++) {
+		for (int i = 0; i < feld.length; i++) {
 
-		            for (int  j =0; j <feld[i].length; j++) {
-		            	str=""+ch+x;
-		            	x++;
-		           
-		           
-		            	
-		            	Spielfeld spielfeld = new Spielfeld(sb, str,true);
-		            	spielfeld.setFarbeFeld(FarbEnum.SCHWARZ);
-		            	System.out.print(spielfeld);
-		            	
-		            	
-		            	  
-		            	  if(x>12){
-			            		x=1;
-			            		ch--;
-			            		if(ch==ch1){
-			            			str=""+ch1+x;
-			            		
-			            			break;
-			            			
-			            		}
-			            		 
-		            }
-		            	  }
-		            }
-			 
-			 }
-	
+			for (int j = 0; j < feld[i].length; j++) {
+				str = "" + ch + x;
+				x++;
+
+				Spielfeld spielfeld = new Spielfeld(sb, str, schwarz);
+				// spielfeld.setFarbeFeld(FarbEnum.SCHWARZ);
+				System.out.print(spielfeld);
+
+				schwarz = !schwarz;
+
+				if (x > 12) {
+					x = 1;
+					ch--;
+					if (ch == ch1) {
+						str = "" + ch1 + x;
+						schwarz = !schwarz;
+
+						break;
+
+					}
+					schwarz = !schwarz;
+
+				}
+				
+			}
+			
+		}
 		
-	
-	public boolean getFarbe(){
-		return schwarz;
-	
 	}
-		
-public static void main(String[] args) {
+
+	/**
+	 * Getter fuer die Farbe
+	 * @return schwarz
+	 */
+	public boolean getFarbe() {
+		return schwarz;
+
+	}
+	
+	
+
+	@Override
+	public String toString() {
+		return "";
+	}
+
+	public static void main(String[] args) {
 
 		Spielbrett s = new Spielbrett();
 
 		System.out.println(s);
 	}
-
 
 }
